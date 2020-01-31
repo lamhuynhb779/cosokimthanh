c52b31df9352887c0717a0a61ff288e82d309924
lamhuynhb779 <lamhuynhb779@gmail.com>
2020-02-01 00:55:12 +0700

    init

---
+++ blocks/nav.php
@@ -0,0 +1,10 @@
+<nav class="top-nav main-menu">
+	<ul class="top-nav">
+		<li><a href="">Trang chủ</a><span> </span></li>
+		<li><a href="gioi-thieu.html">Giới thiệu</a><span> </span></li>
+		<li><a href="products.html">Sản phẩm</a><span> </span></li>
+		<li><a href="lien-he.html">Liên hệ</a></li>
+		<div class="clearfix"> </div>
+	</ul>
+	<a href="#" id="pull"><img src="images/nav-icon.png" title="menu" /></a>
+</nav>
