ca552b059261ad308ec3608f9967af738d220238
lamhuynhb779 <lamhuynhb779@gmail.com>
2020-02-01 17:05:41 +0700

    update catalogies

--- blocks/catalogies.php
+++ blocks/catalogies.php
@@ -1,33 +1,34 @@
 <div class="top-row">
 	<div class="col-xs-4">
 		<div class="top-row-col1 text-center">
-			<h2>WOMAN</h2>
-			<img class="r-img text-center" src="images/img-w.jpg" title="name" />
+			<h3>NẮP XANH</h3>
+			<img class="r-img text-center" src="images/cata-napxanh.png" title="name" />
 			<span><img src="images/obj1.png" title="name" /></span>
-			<h4>TOTAL</h4>
-			<label>357 PRODUCTS</label>
+			<h4>SỐ LOẠI</h4>
+			<label>1 loại</label>
 			<a class="r-list-w" href="single-page.php"><img src="images/list-icon.png" title="list" /></a>
 		</div>
 	</div>
 	<div class="col-xs-4">
 		<div class="top-row-col1 text-center">
-			<h2>MAN</h2>
-			<img class="r-img text-center" src="images/man-r-img.jpg" title="name" />
-			<span><img src="images/obj2.png" title="name" /></span>
-			<h4>TOTAL</h4>
-			<label>357 PRODUCTS</label>
+			<h3>NẮP TƯƠNG ỚT</h3>
+			<img class="r-img text-center" src="images/cata-napvangdep.png" title="name" />
+			<span><img src="images/obj1.png" title="name" /></span>
+			<h4>SỐ LOẠI</h4>
+			<label>2 loại</label>
 			<a class="r-list-w" href="single-page.php"><img src="images/list-icon.png" title="list" /></a>
 		</div>
 	</div>
 	<div class="col-xs-4">
 		<div class="top-row-col1 text-center">
-			<h2>KIDS</h2>
-			<img class="r-img text-center" src="images/kid-r-img.jpg" title="name" />
-			<span><img src="images/obj3.png" title="name" /></span>
-			<h4>TOTAL</h4>
-			<label>357 PRODUCTS</label>
+			<h3>RỔ</h3>
+			<img class="r-img text-center" src="images/cata-ro.png" title="name" />
+			<span><img src="images/obj1.png" title="name" /></span>
+			<h4>SỐ LOẠI</h4>
+			<label>3 loại</label>
 			<a class="r-list-w" href="single-page.php"><img src="images/list-icon.png" title="list" /></a>
 		</div>
 	</div>
-	<vdi class="clearfix"> </div>
+	<vdi class="clearfix">
+</div>
 </div>
